a2401faa4e5c79841dae885daa80f1d59b679975
JuanG07 <123901166+JuanG07@users.noreply.github.com>
2023-01-30 21:48:46 -0300

    Add files via upload

---
+++ Curriculum_Vitae.js
@@ -0,0 +1,41 @@
+let menuVisible = false;
+//Función que oculta o muestra el menú
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //Oculto el menu una vez que selecciono una opción
+    document.getElementById("nav").classList="";
+    menuVisible = false;
+}
+
+//Función que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var habilidades = document.getElementById("habilidades");
+    var distancia_habilidades = window.innerHeight - habilidades.getBoundingClientRect().top;
+    if(distancia_habilidades >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupal");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+//Detecto el scrolling para aplicar las animación de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+}
